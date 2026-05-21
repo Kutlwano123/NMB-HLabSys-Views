a1864b932d23dfa3fe624ad6bb918b0d0d11acaa
Kutlwano Modise <s225579693@mandela.ac.za>
2026-05-22 01:12:34 +0200

    yeah

---
+++ NMB-HLabSys(VIEWS)/Controllers/LabManagerController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NMB_HLabSys_VIEWS_.Controllers
+{
+    public class LabManagerController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+        public IActionResult Consumables()
+        {
+            return View("Consumables/Index");
+        }
+        public IActionResult ConsumableDetails()
+        {
+            return View("Consumables/Details");
+        }
+        public IActionResult CreateConsumable()
+        {
+            return View("Consumables/Create");
+        }
+        public IActionResult EditConsumable(int id)
+        {
+            ViewBag.ConsumableId = id;
+            return View("Consumables/Edit");
+        }
+        public IActionResult DeleteConsumable(int id)
+        {
+            ViewBag.ConsumableId = id;
+            return View("Consumables/Delete");
+        }
+    }
+}
