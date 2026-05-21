766d9a32f1e8e5f1c9627cb069dca39510a6b0f3
Kutlwano Modise <s225579693@mandela.ac.za>
2026-05-22 00:30:15 +0200

    Add project files.

---
+++ NMB-HLabSys(VIEWS)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NMB_HLabSys_VIEWS_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
