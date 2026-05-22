a9aedb39b1ce95bfd4d66447117982c69dd23241
Kutlwano Modise <s225579693@mandela.ac.za>
2026-05-22 03:11:53 +0200

    yes

--- NMB-HLabSys(VIEWS)/Controllers/LabManagerController.cs
+++ NMB-HLabSys(VIEWS)/Controllers/LabManagerController.cs
@@ -30,5 +30,147 @@ namespace NMB_HLabSys_VIEWS_.Controllers
             ViewBag.ConsumableId = id;
             return View("Consumables/Delete");
         }
+        public IActionResult TestTypes()
+        {
+            return View("TestTypes/Index");
+        }
+        public IActionResult TestTypeDetails()
+        {
+            return View("TestTypes/Details");
+        }
+        public IActionResult CreateTestType()
+        {
+            return View("TestTypes/Create");
+        }
+        public IActionResult EditTestType()
+        {
+            return View("TestTypes/Edit");
+        }
+        public IActionResult DeleteTestType()
+        {
+            return View("TestTypes/Delete");
+        }
+        public IActionResult TestCategories()
+        {
+            return View("TestCategories/Index");
+        }
+        public IActionResult TestCategoryDetails()
+        {
+            return View("TestCategories/Details");
+        }
+        public IActionResult DeleteTestCategory()
+        {
+            return View("TestCategories/Delete");
+        }
+        public IActionResult CreateTestCategory()
+        {
+            return View("TestCategories/Create");
+        }
+        public IActionResult EditTestCategory()
+        {
+            return View("TestCategories/Edit");
+        }
+        public IActionResult Suppliers()
+        {
+            return View("Suppliers/Index");
+        }
+        public IActionResult CreateSupplier()
+        {
+            return View("Suppliers/Create");
+        }
+        public IActionResult EditSupplier()
+        {
+            return View("Suppliers/Edit");
+        }
+        public IActionResult DeleteSupplier()
+        {
+            return View("Suppliers/Delete");
+        }
+        public IActionResult SupplierDetails()
+        {
+            return View("Suppliers/Details");
+        }
+        public IActionResult Doctors()
+        {
+            return View("Doctors/Index");
+        }
+        public IActionResult CreateDoctor()
+        {
+            return View("Doctors/Create");
+        }
+        public IActionResult EditDoctor()
+        {
+            return View("Doctors/Edit");
+        }
+        public IActionResult DeleteDoctor()
+        {
+            return View("Doctors/Delete");
+        }
+        public IActionResult DoctorDetails()
+        {
+            return View("Doctors/Details");
+        }
+        public IActionResult LabTechnicians()
+        {
+            return View("LabTechnicians/Index");
+        }
+        public IActionResult CreateLabTechnician()
+        {
+            return View("LabTechnicians/Create");
+        }
+        public IActionResult EditLabTechnician()
+        {
+            return View("LabTechnicians/Edit");
+        }
+        public IActionResult DeleteLabTechnician()
+        {
+            return View("LabTechnicians/Delete");
+        }
+        public IActionResult LabTechnicianDetails()
+        {
+            return View("LabTechnicians/Details");
+        }
+        public IActionResult SampleTypes()
+        {
+            return View("SampleTypes/Index");
+        }
+        public IActionResult CreateSampleType()
+        {
+            return View("SampleTypes/Create");
+        }
+
+        public IActionResult EditSampleType()
+        {
+            return View("SampleTypes/Edit");
+        }
+        public IActionResult DeleteSampleType()
+        {
+            return View("SampleTypes/Delete");
+        }
+        public IActionResult SampleTypeDetails()
+        {
+            return View("SampleTypes/Details");
+        }
+        public IActionResult Orders()
+        {
+            return View("Orders/Index");
+        }
+        public IActionResult CreateOrder()
+        {
+            return View("Orders/Create");
+        }
+        public IActionResult EditOrder()
+        {
+            return View("Orders/Edit");
+        }
+        public IActionResult DeleteOrder()
+        {
+            return View("Orders/Delete");
+        }
+        public IActionResult OrderDetails()
+        {
+            return View("Orders/Details");
+        }
     }
+
 }
