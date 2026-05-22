5f462dc87c50bc1fba9eecdd968175375f938722
NtandoSelekane5 <ntandoselekane5@gmail.com>
2026-05-22 02:30:27 +0200

    Ntando changes

---
+++ NMB-HLabSys(VIEWS)/Controllers/PatientController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NMB_HLabSys.Controllers
+{
+    public class PatientController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+        public IActionResult MedicalHistory()
+        {
+            return View();
+        }
+
+        public IActionResult TestRequests()
+        {
+            return View();
+        }
+
+        public IActionResult ViewResult(int id)
+        {
+            ViewBag.ResultId = id;
+            return View();
+        }
+
+        public IActionResult Consent()
+        {
+            return View();
+        }
+
+        public IActionResult Reports()
+        {
+            return View();
+        }
+    }
+}
